e493f49b047deac5e1e7e007af5e82f048b276f1
pabitra <89703515+appu900@users.noreply.github.com>
2024-05-23 02:24:05 +0530

    completed basic sequalize setup

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,3 +5,17 @@ dotenv.config();
 module.exports = {
     PORT:process.env.PORT
 }
+
+
+
+
+
+class UserController{
+    constructor(dataModel){
+        this.dataModel = dataModel;
+    }
+
+    async create(data){
+        
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 
-const {PORT} = require("./config/serverConfig")
+const { PORT } = require("./config/serverConfig");
 
-
-
-const setupAndStartServer = async() => {
+const setupAndStartServer = async () => {
 
   // ** create the express project
   const app = express();
-  const PORT = process.env.PORT || 3000;
 
-
+  // ** Middlewares
+  app.use(express.json());
+  
   app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
   });
-
-}
+};
 
 setupAndStartServer();
+
+
+
+
 
 
 
